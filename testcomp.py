41246105ba0904e30ef58f7c9297eea6cd2499c6
root <root@localhost.localdomain>
2019-02-03 00:34:04 +0100

    Conversion complete page HTML fonctionnel ajout de la fonction title

--- testcomp.py
+++ testcomp.py
@@ -15,10 +15,14 @@ for files in os.listdir(path):
 @click.command()
 @click.option("-i", "--input_file", default = '', help = "path of input file")
 @click.option("-o", "--output_file", default = '', help = "Path of output file")
+@click.option("-t", "--title", default = 'site-statique', help = "Title of your website")
 
-
-def convert_test(input_file, output_file):
+def convert_test(input_file, output_file, title):
+	
+	HTML_start = '<!DOCTYPE html>\n<html>\n<head>\n<title>' + title + '</title>\n</head>\n<body>\n' 
+	HTML_end = '</body>\n</html>'
 	file = input_file
+	
 	if file[0] == "'" or file[0] == '"':
 		print('bad format of input')
 		time.sleep(1)
@@ -27,9 +31,9 @@ def convert_test(input_file, output_file):
 		convert_file = open(file, mode='r', encoding="utf-8")
 		text = convert_file.read()
 		html = markdown2.markdown(text)
-		f = open(output_file, "w+")
-		f.write(html)
-		f.close
+		output = open(output_file, "w+")
+		output.write(HTML_start + html + HTML_end)
+		output.close
 
 if __name__ == '__main__':
 	convert_test()
